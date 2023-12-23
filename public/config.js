71ae31da27164c9fc8d9e247ec0f11b704444631
clover1420 <748883120@qq.com>
2023-12-23 21:01:22 +0800

    -add 修改样式内容

---
+++ public/config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '灯火阑珊-网站监测',
+
+  CopyrightYear: 2023,
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm794585743-87547bf0f577dd175da4bece',
+    'm794585762-c180005c6e32f7990dc456f7',
+    'm795976775-4e10c1d0b68fa0f85c9f4bad',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'GitHub',
+      url: 'https://github.com/clover1420'
+    },
+    {
+      text: '博客',
+      url: 'https://fhrf.top/'
+    },
+  ],
+};
